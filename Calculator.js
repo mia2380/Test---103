dbfb40f4f5e05dd1b14c9567d7b79ed1d43e34f2
mia2380 <112995643+mia2380@users.noreply.github.com>
2022-10-01 20:08:26 -0400

    Assignment 3_Calculator
    October 1, 2022

--- Calculator.js
+++ Calculator.js
@@ -3,10 +3,26 @@ function calculator(){
     let num1 = Number(prompt("Enter num1:"));
     let num2 = Number(prompt("Enter num2:"));
     sum(num1,num2);
+    multiply(num1,num2);
+    substract(num1,num2);
+    divide(num1,num2);
 
 }
 
 function sum(a,b){
     let total = a + b;
-    document.getElementById("sumRes").innerHTML=`<p> ${a} + ${6} = ${total}</p>`;
+    document.getElementById("sumRes").innerHTML=`<p> ${a} + ${b} = ${total}</p>`;
+}
+function multiply(a,b){
+    let total = a * b;
+    document.getElementById("mulRes").innerHTML=`<p> ${a} * ${b} = ${total}</p>`;
+}
+function substract(a,b){
+    let total = a - b;
+    document.getElementById("subRes").innerHTML=`<p> ${a} - ${b}
+    = ${total}</p>`;
+}
+function divide(a,b){
+    let total = a / b;
+    document.getElementById("divRes").innerHTML=`<p> ${a} / ${b} = ${total}</p>`;
 }
